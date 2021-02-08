a995fbdf26f74505a52a8a16301ec557cc81fddc
Kuprienko Viktoria <vikkytorry1@gmail.com>
2021-02-09 01:36:10 +0200

    Add functional for render genres

--- controllers/mobile/frmMovieListController/frmMovieListController.js
+++ controllers/mobile/frmMovieListController/frmMovieListController.js
@@ -1,7 +1,7 @@
 define({
   onInitialize: function() {
-    //     this.view.postShow = this.onFormShowed.bind(this);
-    //  this.view.lstMovies.onRowClick = Utility.navigateTo.bind(null, "frmMovieDetails");
+//     this.view.postShow = this.onFormShowed.bind(this);
+//  this.view.lstMovies.onRowClick = Utility.navigateTo.bind(null, "frmMovieDetails");
     this.view.lstMovies.onRowClick = this.onRowClicked.bind(this);
     this.view.btnProfile.onClick = Utility.navigateTo.bind(null, "frmAuthentication");
     this.view.btnPopular.onClick = this.loadMovieList.bind(this, "popular");
@@ -9,42 +9,56 @@ define({
     this.view.btnInTheatres.onClick = this.loadMovieList.bind(this, "now_playing");
     this.view.btnUpcoming.onClick = this.loadMovieList.bind(this, "upcoming");
   },
-
-  onNavigate: function() {
-    this.loadMovieList("popular");
+  
+	onNavigate: function() {
+		this.loadMovieList("popular");
   },
-
+	
   onRowClicked: function(widgetRef, sectionIndex, rowIndex) {
     Utility.navigateTo("frmMovieDetails", widgetRef.data[rowIndex].id);
   },
-
-  //   onFormShowed: function() {
-  //     this.loadMovieList.bind(this, "popular");
-  //   },
-
+  
+//   onFormShowed: function() {
+//     this.loadMovieList.bind(this, "popular");
+//   },
+  
   loadMovieList: function(url) {
     kony.application.showLoadingScreen();
     var httpClient = new kony.net.HttpRequest();
     httpClient.open(constants.HTTP_METHOD_GET, "https://api.themoviedb.org/3/movie/" + url +"?api_key=69f776e126f6211fe76798c6c4b786f9&language=en-US&page=1");
-    httpClient.onReadyStateChange = this.onMovieListReceived.bind(this, httpClient);
+    var genreHttpClient = new kony.net.HttpRequest();
+    genreHttpClient.open(constants.HTTP_METHOD_GET, "https://api.themoviedb.org/3/genre/movie/list?api_key=69f776e126f6211fe76798c6c4b786f9&language=en-US");
+    httpClient.onReadyStateChange = this.onMovieListReceived.bind(this, httpClient, genreHttpClient);
+    genreHttpClient.onReadyStateChange = this.onMovieListReceived.bind(this, httpClient, genreHttpClient);
     httpClient.send();
-    //     this.view[btn].skin = "sknBtnNavigateActive";
+    genreHttpClient.send();
+//     this.view[btn].skin = "sknBtnNavigateActive";
   },
-
-  onMovieListReceived: function(httpClient) {
+  
+  onMovieListReceived: function(httpClient, genreHttpClient) {
     kony.print("Movie List Retrieval State Change: " + httpClient.readyState);
 
-    if(httpClient.readyState !== constants.HTTP_READY_STATE_DONE) {
+    if(httpClient.readyState !== constants.HTTP_READY_STATE_DONE && genreHttpClient.readyState !== constants.HTTP_READY_STATE_DONE) {
       return;
     }
-
-
+    
+    var genreData = genreHttpClient.response;
     var movieData = httpClient.response;
     var listData = movieData.results.map(function(m) {
+      var genres = m.genre_ids.map(function(g) {
+        var genre = genreData.find(function(elem){
+          return elem.id === g.id;
+        });
+        if(genre){
+          return genre.name;
+        }
+        return '';
+      });
+      
       return {
         id: m.id,
         lblMovieTitle: m.title,
-        lblMovieDescription: m.overview,
+        lblMovieGenres: genres.join(' '),
         imgMoviePoster: "https://image.tmdb.org/t/p/w200/" + m.poster_path
       };
     });
