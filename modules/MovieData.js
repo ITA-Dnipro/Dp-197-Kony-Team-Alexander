31490b2a8906953313b21cabc225abf21f6cc383
Kuprienko Viktoria <vikkytorry1@gmail.com>
2021-02-09 22:06:12 +0200

    Feat added active skins for footer buttons

--- modules/MovieData.js
+++ modules/MovieData.js
@@ -1,5 +1,6 @@
-function MovieData(id, title, description, genresId, posterPath, voteAvg, genreNamesList) {
+function MovieData(id, title, description, genresId, posterPath, voteAvg, release_date, genreNamesList) {
   var poster = "https://image.tmdb.org/t/p/w200/" + posterPath;
+  var year = release_date.slice(0,4);
   
   this.id = id || 0;
   this.title = title || 0;
@@ -7,5 +8,6 @@ function MovieData(id, title, description, genresId, posterPath, voteAvg, genreN
   this.genresList = genresId || 0;
   this.voteAvg = voteAvg || 0;
   this.poster = poster || 0;
+  this.year = year || 0;
   this.genreNamesList = genreNamesList || 0;
 }
