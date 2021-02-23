0784f4a111d7365e2d00100cd55aa2ada24499ad
Alla Rakhimova <alla.rakhimova103@gmail.com>
2021-02-23 14:11:43 +0200

    fix: hide top billed cast container if there isn't anyone

--- controllers/mobile/frmMovieDetailsController/frmMovieDetailsController.js
+++ controllers/mobile/frmMovieDetailsController/frmMovieDetailsController.js
@@ -10,7 +10,7 @@ define(["MovieService", "AuthenticationService"], function(movieService, dbServi
     onbtnFavoriteClicked: function() {
       this.view.btnFavorite.skin === "sknBtnFavorite" ?
         this.view.btnFavorite.skin = "sknBtnFavoriteActive" :
-      this.view.btnFavorite.skin = "sknBtnFavorite";
+        this.view.btnFavorite.skin = "sknBtnFavorite";
 
       dbService.addDeleteMovieFavorites(this.movieId);
     },
@@ -123,70 +123,81 @@ define(["MovieService", "AuthenticationService"], function(movieService, dbServi
     onMovieCreditsReceived: function(creditsList) {
       this.view.flxCastCarousel.removeAll();
 
-      for (var i = 0; i < creditsList.cast.length; i++) {
-        var flexCast = new kony.ui.FlexContainer({
-          id: "flxCast" + i,
-          top: "0dp",
-          left: "5dp",
-          width: "130dp",
-          height: kony.flex.USE_PREFERRED_SIZE,
-          //           onClick: function(){
-          //             alert("container ");
-          //           },
-          //           onClick: this.onPeopleClicked.bind(null, creditsList.cast[i].id),
-          layoutType: kony.flex.FLOW_VERTICAL
-        });
+      alert('cast ' + creditsList.cast.length);
 
-        var imgCast = new kony.ui.Image2({
-          id: "imgCast" + i,
-          src: creditsList.cast[i].img,
-          top: "0dp",
-          width: "100%",
-          height: "130dp",
+      if (creditsList.cast.length === 0) {
+        this.view.flxCastCarousel.isVisible = false;
+        this.view.lblTopCast.isVisible = false;
 
-          //           onTouchEnd: function() {
-          //             alert("touch end " + creditsList.cast[i].id);
-          //           } 
-        });
+      } else {
+        this.view.flxCastCarousel.isVisible = true;
+        this.view.lblTopCast.isVisible = true;
+        // creditsList.cast.length
+        for (var i = 0; i < 20; i++) {
+          var flexCast = new kony.ui.FlexContainer({
+            id: "flxCast" + i,
+            top: "0dp",
+            left: "5dp",
+            width: "130dp",
+            height: kony.flex.USE_PREFERRED_SIZE,
+            //           onClick: function(){
+            //             alert("container ");
+            //           },
+            //           onClick: this.onPeopleClicked.bind(null, creditsList.cast[i].id),
+            layoutType: kony.flex.FLOW_VERTICAL
+          });
 
-        //         imgCast.addGestureRecognizer(1, { fingers: 1, taps: 1 }, onActorImageTap);
+          var imgCast = new kony.ui.Image2({
+            id: "imgCast" + i,
+            src: creditsList.cast[i].img,
+            top: "0dp",
+            width: "100%",
+            height: "130dp",
 
-        //         var onActorImageTap = function() {
-        //           alert("img tap " + creditsList.cast[i].id);
-        //         };
+            //           onTouchEnd: function() {
+            //             alert("touch end " + creditsList.cast[i].id);
+            //           } 
+          });
 
-        var btnName = new kony.ui.Button({
-          id: "lblCastName" + i,
-          text: creditsList.cast[i].name,
-          top: "5dp",
-          left: "0dp",
-          width: "100%",
-          height: kony.flex.USE_PREFERRED_SIZE,
-          isVisible: true,
-          skin: "sknCastBtnName",
-          onClick: this.onPeopleClicked.bind(null, creditsList.cast[i].id)
-        }, {
-          padding: [0,0,0,0],
-          margin: [0,0,0,0],
-          contentAlignment: constants.CONTENT_ALIGN_CENTER,
-        });
+          //         imgCast.addGestureRecognizer(1, { fingers: 1, taps: 1 }, onActorImageTap);
 
-        var lblChar = new kony.ui.Label({
-          id: "lblCastCharacter" + i,
-          text: creditsList.cast[i].character,
-          skin: "sknCastCharacter",
-          top: "5dp",
-          left: "0dp",
-          width: "100%",
-          height: kony.flex.USE_PREFERRED_SIZE,
-          contentAlignment: constants.CONTENT_ALIGN_CENTER,
-          wrapping: constants.WIDGET_TEXT_WORD_WRAP
-        });
+          //         var onActorImageTap = function() {
+          //           alert("img tap " + creditsList.cast[i].id);
+          //         };
 
-        flexCast.add(imgCast, btnName, lblChar);
+          var btnName = new kony.ui.Button({
+            id: "lblCastName" + i,
+            text: creditsList.cast[i].name,
+            top: "5dp",
+            left: "0dp",
+            width: "100%",
+            height: kony.flex.USE_PREFERRED_SIZE,
+            isVisible: true,
+            skin: "sknCastBtnName",
+            onClick: this.onPeopleClicked.bind(null, creditsList.cast[i].id)
+          }, {
+            padding: [0,0,0,0],
+            margin: [0,0,0,0],
+            contentAlignment: constants.CONTENT_ALIGN_CENTER,
+          });
 
-        this.view.flxCastCarousel.add(flexCast);
-      }		
+          var lblChar = new kony.ui.Label({
+            id: "lblCastCharacter" + i,
+            text: creditsList.cast[i].character,
+            skin: "sknCastCharacter",
+            top: "5dp",
+            left: "0dp",
+            width: "100%",
+            height: kony.flex.USE_PREFERRED_SIZE,
+            contentAlignment: constants.CONTENT_ALIGN_CENTER,
+            wrapping: constants.WIDGET_TEXT_WORD_WRAP
+          });
+
+          flexCast.add(imgCast, btnName, lblChar);
+
+          this.view.flxCastCarousel.add(flexCast);
+        }		
+      }
     },
 
     onPeopleClicked: function(id) {
