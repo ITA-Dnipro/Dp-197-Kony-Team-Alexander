140db4a214c0678d6735f91b4d21799b4180a5af
MishaNovikov <mishanovikov14@gmail.com>
2021-02-25 14:41:42 +0200

    feat: added delete functionality to favouriteList

--- controllers/mobile/frmFavouriteListController/frmFavouriteListController.js
+++ controllers/mobile/frmFavouriteListController/frmFavouriteListController.js
@@ -1,3 +1,61 @@
-define({ 
- 
+define(["MovieService"], function(movieService){
+  return {
+    onInitialize: function() {
+      this.view.lstMovies.onRowClick = this.onRowClicked.bind(this);
+      this.view.btnBack.onClick = Utility.navigateTo.bind(null, "frmMovieList");
+    },
+
+    onNavigate: function() {  
+      kony.application.showLoadingScreen();
+
+      movieService.getMovieList(function(movieList) {
+        this.onMovieListReceived(movieList);
+      }.bind(this), function() {
+        alert("Error while retrieving movie list");
+        kony.application.dismissLoadingScreen();
+      }, "popular");
+    },
+
+    loadMovieList: function(url) {
+      kony.application.showLoadingScreen();
+
+      movieService.getMovieList(function(movieList) {
+        this.onMovieListReceived(movieList);
+      }.bind(this), function() {
+        alert("Error while retrieving movie list");
+        kony.application.dismissLoadingScreen();
+      }, url);
+    },
+
+    onRowClicked: function(widgetRef, sectionIndex, rowIndex) {
+      Utility.navigateTo("frmMovieDetails", {id: widgetRef.data[rowIndex].id});
+    },
+
+    onMovieListReceived: function(movieList) {
+            var lst = this.view.lstMovies;
+      var movieListData = movieList.map(function(m) {
+        return {
+          lblMovieTitle: m.title,
+          lblMovieDescription: m.genreNamesList.join(', '),
+          lblMovieYear: String(m.released),
+          imgMoviePoster: m.poster,
+          id: m.id,
+          btnDeleteMoviFromList: {
+            text: "\uf00d",
+            onClick: function() {
+              lst.removeAt(arguments[1].rowIndex);
+            }
+          }
+        };
+      });
+
+      this.view.lstMovies.setData(movieListData);
+      kony.application.dismissLoadingScreen();
+    },
+    
+//     deleteRow: function(rowIndex) {
+//       alert(rowIndex);
+//       this.view.lstMovies.removeAt(rowIndex);
+//     }
+  };
 });
