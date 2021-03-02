4ed7d58b6fdf9cb6cb4b2ddf070485df319b7a67
MishaNovikov <mishanovikov14@gmail.com>
2021-03-02 15:44:14 +0200

    feat: added UserProfileService with design fixes

--- modules/require/AuthServiceFabricImpl.js
+++ modules/require/AuthServiceFabricImpl.js
@@ -1,14 +1,14 @@
 define(function () {
   var registerUser = function(fullName, login, password, successCB, errorCB) {
     var sdk = kony.sdk.getCurrentInstance();
-    var AlexanderMovieListService = sdk.getIntegrationService("AlexDB");
+    var AlexanderAuthService = sdk.getIntegrationService("AlexDB");
     var headers = null;
     var body = { 
       userFullName: fullName,
       userLogin: login,
       userPassword: password
     };
-    AlexanderMovieListService.invokeOperation("registerUser", headers, body, function(response) {
+    AlexanderAuthService.invokeOperation("registerUser", headers, body, function(response) {
       if (response.userNewId) {
         alert("User was created!");
         successCB();
@@ -31,71 +31,31 @@ define(function () {
   var checkUser = function(login, password, successCB, errorCB) {
 
     var sdk = kony.sdk.getCurrentInstance();
-    var AlexanderMovieListService = sdk.getIntegrationService("AlexDB");
+    var AlexanderAuthService = sdk.getIntegrationService("AlexDB");
     var headers = null;
     var body = {
       userLogin: login,
       userPassword: password
     };
-    AlexanderMovieListService.invokeOperation("loginUser", headers, body, function(response) {
+    AlexanderAuthService.invokeOperation("loginUser", headers, body, function(response) {
       if (response.userLoginId) {
         successCB(response.userLoginId);
       } else {
-        alert("User not found. Please registrate.");
+        var result = response.userLoginError;
+        switch(result) {
+          case "login_required": alert("Login is required."); break;
+          case "password_required": alert("Password is required."); break;
+          case "login_failed": alert("User not found, please try again."); break;
+        }
       }
     }, function(error) {
       if (errorCB) {
         errorCB(error);
       }
     }); 
-  };
-
-  var getUserProfile = function(userId, successCB, errorCB) {
-
-    var sdk = kony.sdk.getCurrentInstance();
-    var AlexanderMovieListService = sdk.getIntegrationService("AlexDB");
-    var headers = null;
-    var body = {
-      userId: userId
-    };
-    AlexanderMovieListService.invokeOperation("getUserProfile", headers, body, function(response) {
-      if (response.userLogin && response.userFullName) {
-        successCB(response.userLogin, response.userFullName);
-      } else {
-        alert("User not found");
-      }
-    }, function(error) {
-      if (errorCB) {
-        errorCB(error);
-      }
-    }); 
-  };
-
-  var updateUserProfile = function(userId, fullName, successCB, errorCB) {
-
-    var sdk = kony.sdk.getCurrentInstance();
-    var AlexanderMovieListService = sdk.getIntegrationService("AlexDB");
-    var headers = null;
-    var body = {
-      userId: userId,
-      userFullName: fullName
-    };
-    AlexanderMovieListService.invokeOperation("updateUserProfile", headers, body, function(response) {
-      if (successCB) {
-        successCB(response.userLogin, response.userFullName);
-      } else {
-        alert("User not found");
-      }
-    }, function(error) {
-      if (errorCB) {
-        errorCB(error);
-      }
-    });
   };
 
   return {
-    updateUserProfile: updateUserProfile,
-    getUserProfile: getUserProfile,
     checkUser: checkUser,
     registerUser: registerUser
   };
