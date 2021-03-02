197326e84f1c1f86ad95ae969d33a7cb3df1dd6a
MishaNovikov <mishanovikov14@gmail.com>
2021-03-02 11:48:37 +0200

    fix: added showing empty lbl to favList

--- controllers/mobile/frmFavouriteListController/frmFavouriteListController.js
+++ controllers/mobile/frmFavouriteListController/frmFavouriteListController.js
@@ -3,8 +3,6 @@ define(["FavouriteListService"], function(favouriteListService){
     onInitialize: function() {
       this.view.lstMovies.onRowClick = this.onRowClicked.bind(this);
       this.view.btnBack.onClick = Utility.goBack;
-//       this.view.btnBack.onClick = Utility.navigateTo.bind(null, "frmMovieList");
-      
       this.view.onDeviceBack = Utility.goBack;
     },
 
@@ -24,7 +22,13 @@ define(["FavouriteListService"], function(favouriteListService){
     },
 
     onMovieListReceived: function(movieList) {
+      if (movieList.length < 1) {
+        this.view.lblIsEmpty.setVisibility(true);
+      } else {
+        this.view.lblIsEmpty.setVisibility(false);
+      }
       var lst = this.view.lstMovies;
+      var lblIsEmpty = this.view.lblIsEmpty;
       var movieListData = movieList.map(function(m) {
         return {
           lblMovieTitle: m.title,
@@ -41,10 +45,19 @@ define(["FavouriteListService"], function(favouriteListService){
               lst.removeAt(rowIndex);
               favouriteListService.getFavouriteMovies(UserId, function(favList) {
                 favouriteListService.deleteFavouriteList(favList[rowIndex].dbId, function() {
+                  favouriteListService.getFavouriteMovies(UserId, function(finalFavList) {
+                    if (finalFavList.length < 1) {
+                      lblIsEmpty.setVisibility(true);
+                    } else {
+                      lblIsEmpty.setVisibility(false);
+                    }
+                  }, function() {
+                    alert("Error while retrieving favourite movie list");
+                  });
                   alert("Deleted movie from favourite list");
                 }, function() {
-                alert("Error while deleting favourite movie list");
-              });
+                  alert("Error while deleting favourite movie list");
+                });
               }, function() {
                 alert("Error while retrieving favourite movie list");
               });
