9f2dd077fc6b0723167cb5ed75f6e8c4a76d17d4
Kuprienko Viktoria <vikkytorry1@gmail.com>
2021-03-03 20:07:58 +0200

    fix: render the map on form

--- controllers/mobile/frmNearestCinemasController/frmNearestCinemasController.js
+++ controllers/mobile/frmNearestCinemasController/frmNearestCinemasController.js
@@ -2,19 +2,38 @@ define(["MapServiceFabricImpl"], function(){
   return { 
     onInitialize: function() {
     this.view.BackHeader.btnBackHeader.onClick = Utility.goBack; 
+    this.view.mapCinemaWidget.showCurrentLocation = true;
+    this.view.btnGetLocation.onClick = this.onGetLocationClicked.bind(this);   
     },
     
-    getLocation: function(){
-      //Sample code to set the locationData property of a Map widget.
-      frmNearestCinemas.mapWidget.locationData = [{
-        lat: "17.445775",
-        lon: "78.3731",
-        name: "Campus 1",
-        desc: "My Office Campus",
-        color: "green",
-        label: "C",
-      }];
-    }
+    onGetLocationClicked: function(){
+      kony.location.getCurrentPosition(
+        function(position){
+          alert("Current location:\n" + JSON.stringify(position))
+        }, 
+        function(eror){
+          alert("Cannot read current location:\n" + JSON.stringify(eror))
+        }, 
+        {
+          accuracyMode: constants.ACCURACY_NO_POWER,
+          enableHighAccuracy: false,
+          timeout: 2000,
+          useBestProvider: true,
+        });
+    },
+//     getLocation: function(){
+//       //change path
+//       frmNearestCinemas.mapCinemaWidget.showCurrentLocation = true;
+//       //Sample code to set the locationData property of a Map widget.
+//       frmNearestCinemas.mapCinemaWidget.locationData = [{
+//         lat: "17.445775",
+//         lon: "78.3731",
+//         name: "Campus 1",
+//         desc: "My Office Campus",
+//         color: "green",
+//         label: "C",
+//       }];
+//     }
     
     
   }
