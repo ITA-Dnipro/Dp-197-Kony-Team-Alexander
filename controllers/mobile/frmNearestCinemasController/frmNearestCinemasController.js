c351eeea35f965eb0da9f73dcbfa439f6dcd7875
Kuprienko Viktoria <vikkytorry1@gmail.com>
2021-03-06 01:38:25 +0200

    feat: navigateToLocation

--- controllers/mobile/frmNearestCinemasController/frmNearestCinemasController.js
+++ controllers/mobile/frmNearestCinemasController/frmNearestCinemasController.js
@@ -10,7 +10,11 @@ define(["MapService"], function (nearestCinemas) {
         lblCalloutVicinity: "vicinity",
         //               imgLocation: "image"
       };
-      var mapControl = this.view.mapControl;
+      
+     },
+    
+    onNavigate: function(){
+        var mapControl = this.view.mapControl;
       kony.location.getCurrentPosition(function(position) {
         nearestCinemas.getNearestCinemas(position.coords.latitude, position.coords.longitude, function(cinemaList){
           mapControl.addPins(cinemaList);
@@ -25,27 +29,30 @@ define(["MapService"], function (nearestCinemas) {
         timeout: 2000,
         useBestProvider: true
       });
+   
     },
     
-    onNavigate: function(){
-      var mapControl = this.view.mapControl;
-      kony.location.getCurrentPosition(function(position) {
-         var locationData = { 
-            lat : position.coords.latitude , 
-            lon : position.coords.longitude , 
-            name : "Current location" , 
-            desc : "Current location" }
-          mapControl.navigateToLocation(locationData, showcallout, dropPin) ( locationData , true , true );
-      }, function(error) {
-        alert("Cannot read current location:\n" + JSON.stringify(error));
-      }, {
-        accuracyMode: constants.ACCURACY_NO_POWER,
-        enableHighAccuracy: false,
-        timeout: 2000,
-        useBestProvider: true
-      });
+//     onNavigate: function(){
+//       var mapControl = this.view.mapControl;
+//       kony.location.getCurrentPosition(function(position) {
+//          var locationData = { 
+//             lat : position.coords.latitude , 
+//             lon : position.coords.longitude , 
+//             name : "Current location" , 
+//             desc : "Current location" }
+//           mapControl.navigateToLocation(locationData, showcallout, dropPin) ( locationData , true , true );
+//       }, function(error) {
+//         alert("Cannot read current location:\n" + JSON.stringify(error));
+//       }, {
+//         accuracyMode: constants.ACCURACY_NO_POWER,
+//         enableHighAccuracy: false,
+//         timeout: 2000,
+//         useBestProvider: true
+//       });
      
     }
+   
+});
 
     //     onNavigate: function(){
     //       kony.location.getCurrentPosition(function (position) {
@@ -84,5 +91,3 @@ define(["MapService"], function (nearestCinemas) {
     //                   image: "https://ied.eu/wp-content/uploads/2019/01/horizon-europe.jpg"
     //                 }
     //       }]);
-  };
-});
